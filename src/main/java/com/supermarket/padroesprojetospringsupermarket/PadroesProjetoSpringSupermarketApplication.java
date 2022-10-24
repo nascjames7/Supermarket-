5a4cbc1e1dc33a85fe720478757673baccff9f39
James <james.nascimento@ufrpe.br>
2022-10-24 19:33:24 -0300

    first commit

---
+++ src/main/java/com/supermarket/padroesprojetospringsupermarket/PadroesProjetoSpringSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket.padroesprojetospringsupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSpringSupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSpringSupermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermarket/padroesprojetospringsupermarket/PadroesProjetoSpringSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket.padroesprojetospringsupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringSupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
