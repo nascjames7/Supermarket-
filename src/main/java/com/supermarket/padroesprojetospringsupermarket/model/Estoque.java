1d7ae1dbe2e295a035c5f8f6c0c290b2e3e5ebae
James <james.nascimento@ufrpe.br>
2022-10-25 08:08:42 -0300

    Adição de classes Produto e Estoque.

--- src/main/java/com/supermarket/padroesprojetospringsupermarket/model/Estoque.java
+++ src/main/java/com/supermarket/padroesprojetospringsupermarket/model/Estoque.java
@@ -3,6 +3,7 @@ package com.supermarket.padroesprojetospringsupermarket.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDate;
 
 @Entity
 public class Estoque {
@@ -20,6 +21,57 @@ public class Estoque {
     public Estoque(Produto produto, int quantidadeAtual, int quantidadeMinima) {
         this.produto = produto;
         this.quantidadeAtual = quantidadeAtual;
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    //Altera a quantidade mínima.
+    public void mudarQtdMinima(int qtdMinima){
+
+        this.setQuantidadeMinima(qtdMinima);
+    }
+
+    //Adiciona 10 unidades de produto a qauntidade atual do mesmo.
+    public void reporEstoque(){
+
+        this.setQuantidadeAtual(quantidadeAtual + 10);
+    }
+
+    //Subtrai certe quantidade de produto do estoque.
+    public void darBaixa(int qtdRetirada){
+
+        this.setQuantidadeAtual(quantidadeAtual - qtdRetirada);
+    }
+
+    //Descreve a quantidade atual de um determinado produto.
+    public void relatorio(Estoque estoque){
+
+        System.out.println(LocalDate.now());
+        System.out.println("Produto/(cod): " + this.produto.getNome() + "/" + this.produto.getCodigo());
+        System.out.println("Quantidade Atual: " + this.getQuantidadeAtual());
+        System.out.println("Quantidade Mínima: " + this.getQuantidadeMinima());
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidadeAtual() {
+        return quantidadeAtual;
+    }
+
+    public void setQuantidadeAtual(int quantidadeAtual) {
+        this.quantidadeAtual = quantidadeAtual;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
         this.quantidadeMinima = quantidadeMinima;
     }
 }
